0c86623e35203afb0e139a9deadd490c4503c5df
Giovani Trevisol <giovanitrevisol93@gmail.com>
2020-05-11 22:29:49 -0300

    finalizando projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,42 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:chat_firebase/chat_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   runApp(MyApp());
-  Firestore.instance
-      .collection("col")
-      .document("doc")
-      .setData({"texto": "Giovani"});
+  //teste para saber se esta funcionando
+  //escrevendo dados
+  // Firestore.instance.collection("mensagens").document("msg2").setData({
+  //   "texto": "Tudo bem?",
+  //   "from": "Lara",
+  //   "read": false,
+  // });
+
+//buscando dados e escrevendo (print)
+  // QuerySnapshot snapshot =
+  //     await Firestore.instance.collection('mensagens').getDocuments();
+  // snapshot.documents.forEach((d) {
+  //   print(d.data);
+  // });
+
+  //atualizacao em tempo real
+  // Firestore.instance.collection('mensagens').snapshots().listen((dado) {
+  //   dado.documents.forEach((d) {
+  //     print(d.data);
+  //   });
+  // });
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+        iconTheme: IconThemeData(color: Colors.blue),
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: ChatScreen(),
     );
   }
 }
